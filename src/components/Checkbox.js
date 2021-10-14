fd7a1bdc1990ef286ee877fac41df2b0fd9a7aca
jirapornNgewdang <jiraporn7494@gmail.com>
2021-10-15 00:27:40 +0700

    Add Checkbox
    building the checkbox component

---
+++ src/components/Checkbox.js
@@ -0,0 +1,24 @@
+import { TRUE } from "node-sass";
+import React from "react";
+import { firebase } from "../firebase";
+
+export const Checkbox = ({ id }) => {
+    const archiveTasks = () => {
+        firebase
+        .firestore()
+        .collection('tasks')
+        .doc(id)
+        .update({
+            archived: true,
+        });
+    };
+    return (
+        <div className="checkbox-holder"
+              data-testid="checkbox-action"
+              onClick={() => archiveTasks()}
+              >
+              <span className="checkbox" />
+
+        </div>
+    );
+};
