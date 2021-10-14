4ddf4f998344a94a53f6b47c0ce97eea1b138d28
iceschp <iceschp>
2021-10-14 17:28:56 +0700

    add apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyD3NytE7BncdtOvOSwe1jLvREuEL-csB0Q",
+    authDomain: "meownote-web.firebaseapp.com",
+    projectId: "meownote-web",
+    storageBucket: "meownote-web.appspot.com",
+    messagingSenderId: "823403966533",
+    appId: "1:823403966533:web:814bc092070367dc19332c",
 });
 
 export { firebaseConfig as firebase };
