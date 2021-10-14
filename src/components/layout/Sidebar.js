23ca801336ad6bafb19ffb28646e82118c4e3e51
iceschp <iceschp>
2021-10-14 16:23:17 +0700

    sidebar generic

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,11 @@
 import React from "react";
 
-export const Sidebar = () => <p>I am a sidebar!</p>
+export const Sidebar = () => (
+<div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar_generic">
+        <li>Inbox</li>
+        <li>Today</li>
+        <li>Next 7 days</li>
+    </ul>
+</div>
+);
