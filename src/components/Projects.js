318528f0b4ea91f547b524da9b8891481b15bb07
iceschp <iceschp>
2021-10-16 20:17:40 +0700

    add onClick button

---
+++ src/components/Projects.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useSelectedProjectValue, useProjectValue } from "../context";
+
+export const Projects = ({ activeValue = null }) => {
+    const [ active, setActive ] = useState(activeValue);
+    const { setSelectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectValue();
+
+    return (
+        projects &&
+        projects.map(project => (
+            <li
+            key = { project.projectId }
+            data-doc-id = { project.docId }
+            data-testid = "project-action"
+            role = "button"
+            className = {
+                active === project.projectId ?
+                'active sidebar__project' :
+                'sidebar__project'
+            }
+            onKeyDown = {() => {
+                setActive(project.projectId);
+                setSelectedProject(project.projectId);
+            }}
+            onClick = {() => {
+                setActive(project.projectId);
+                setSelectedProject(project.projectId);
+            }}
+            >
+            I am a project
+            </li>
+        ))
+    );
+};
