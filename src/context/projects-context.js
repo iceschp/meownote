33a45ebf84ad957a85d62964da29b74e5675152e
iceschp <iceschp>
2021-10-16 16:23:12 +0700

    return ProjectsContext

--- src/context/projects-context.js
+++ src/context/projects-context.js
@@ -5,5 +5,12 @@ export const ProjectsContext = createContext();
 export const ProjectsProvider = ({children}) => {
     const { projects, setProjects } = useProjects();
 
-    return ()
-}
+    return (
+        <ProjectsContext.Provider value={{ projects, setProjects }}>
+            { children }
+        </ProjectsContext.Provider>
+    );
+};
+
+export const useProjectsValue = () => useContext(ProjectsContext);
+
