0d49d753ea0aa1118ea0fddf5aefadc30d462ff2
iceschp <iceschp>
2021-10-16 16:16:27 +0700

    export ProjectsContext ProjectsProvider

--- src/context/projects-context.js
+++ src/context/projects-context.js
@@ -1,3 +1,9 @@
 import React, { createContext, useContext } from 'react';
 import { useProjects } from '../hooks';
 
+export const ProjectsContext = createContext();
+export const ProjectsProvider = ({children}) => {
+    const { projects, setProjects } = useProjects();
+
+    return ()
+}
