4f92a893c13dfeacccee4b17eac480e0bd708bae
iceschp <iceschp>
2021-10-21 21:12:51 +0700

    edit indent

--- src/context/selected-project-context.js
+++ src/context/selected-project-context.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 export const SelectedProjectContext = createContext();
 export const SelectedProjectProvider = ({ children }) => {
-const [selectedProject, setSelectedProject] = useState('INBOX');
+    const [selectedProject, setSelectedProject] = useState('INBOX');
 
-return (
-    <SelectedProjectContext.Provider
-        value={{ selectedProject, setSelectedProject }}
-    >
-        {children}
-    </SelectedProjectContext.Provider>
-    );
-};
+    return (
+        <SelectedProjectContext.Provider
+            value={{ selectedProject, setSelectedProject }}
+        >
+            {children}
+        </SelectedProjectContext.Provider>
+        );
+    };
 
 export const useSelectedProjectValue = () => useContext(SelectedProjectContext);
 
