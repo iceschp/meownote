f30e9c223107a3a3cbc7336848471b9ad1d3b199
iceschp <iceschp>
2021-10-23 15:53:10 +0700

    import addTask

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,7 @@ import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
 import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
+import { AddTask } from './AddTask';
 
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
@@ -41,7 +42,7 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
-
+        <AddTask />
     </div>
   );
 };
