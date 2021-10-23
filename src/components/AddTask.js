5a509900023ad6b13d73293b23bf2cf1f68c3e7d
iceschp <iceschp>
2021-10-23 15:39:44 +0700

    add constances

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -8,5 +8,31 @@ export const AddTask = ({
     showAddTaskMain = true, 
     showShouldMain = false, 
     showQuickAddTask, 
-    setShowQuickAddTask ,
-}) => <p>Add Task</p>
+    setShowQuickAddTask,
+}) => {
+    const [ task, setTask ] = useState('');
+    const [ taskDate, setTaskDate ] = useState('');
+    const [ project, setProject ] = useState('');
+    const [ showMain, setShowMain ] = useState(shouldShowMain);
+    const [ showProjectOverlay, setShowProjectOverlay ] = useState(false);
+    const [ showTaskDate, setShowTaskDate ] = useState(false);
+
+    const { selectedProject } = useSelectedProjectValue();
+
+    const addTask = () => {
+        const projectId = project || selectedProject;
+        let collatedDate = '';
+
+        if (projectId === 'TODAY') {
+            collatedDate = moment().format('DD/MM/YYYY');
+        } else if (projectId === 'NEXT_7') {
+            collatedDate = moment()
+                .add(7, 'days')
+                .format('DD/MM/YYYY');
+        }
+    }
+
+    return (
+        
+    );
+}
