d27c98e834731357a68cb76b2a069638a73df239
iceschp <iceschp>
2021-10-23 14:26:33 +0700

    export shouldShow

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { firebase } from "../firebase";
 import { generatePushId } from "../helpers";
 import { useProjectsValue } from "../context";
 
-export const AddProject = () => <p>Hello Add Project!</p>;
+export const AddProject = ({ shouldShow = false }) => {
+    const [ show, setShow ] = useState(shouldShow);
+    const [ projectName, setProjectName ] = useState('');
+
+    const projectId = generatePushId();
+    const { setProjects } = useProjectsValue();
+
+    const AddProject = () =>
+        projectName &&
+        firebase
+            .firestore()
+            .collection('projects')
+            .add({
+                projectId,
+                name: projectName,
+                userId: 'a24f0ccdd660485fab245837b7bb223c'
+            })
+            .then(() => {
+                setProjects([]);
+                setProjectName('');
+                setShow(false);
+            })
+};
