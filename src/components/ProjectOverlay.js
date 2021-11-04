12aef6192931c81bb4a8ac96be5bf05e4cba9fd3
iceschp <iceschp>
2021-11-04 14:56:59 +0700

    add onclick div

--- src/components/ProjectOverlay.js
+++ src/components/ProjectOverlay.js
@@ -15,7 +15,20 @@ export const ProjectOverlay = ({
                 <ul className="project-overlay__list">
                     {projects.map(project =>(
                     <li key={project.projectId} data-testid="projectid-overlay-action">
-                        {project.name}
+                    <div
+                        onClick={() => {
+                            setProject(project.projectId);
+                            setShowProjectOverlay(false);
+                        }}
+                        onKeyDown={() => {
+                            setProject(project.projectId);
+                            setShowProjectOverlay(false);
+                        }}
+                        role="button"
+                        tabIndex={0}
+                        >
+                            {project.name}
+                    </div>
                     </li>
                     ))}
                 </ul>
