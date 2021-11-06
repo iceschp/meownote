55c5c2ffad0d543d09baffef20a8a2d8ae7642b5
jirapornNgewdang <jiraporn7494@gmail.com>
2021-11-06 21:18:20 +0700

    Testing the App
    Testing the App  component

--- src/_tests_/App.spec.js
+++ src/_tests_/App.spec.js
@@ -1,3 +1,12 @@
 import react from "react";
 import { render,cleanup, fireEvent } from '@testing-library/react';
 import { Checkbox } from "../components/Checkbox";
+
+beforeEach(cleanup); // clean meeeeeee!
+
+describe('<App/>',()=> {
+    it('renders the application',()=> {
+        const{queryByTestId,debug} = render(<App />);
+        expect(queryByTestId('application')).toBeTruthy();
+    });
+});
