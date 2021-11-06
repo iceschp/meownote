5f6169e0639081e3a54622a8bf4d5790725b0962
jirapornNgewdang <jiraporn7494@gmail.com>
2021-11-06 19:28:37 +0700

    Modifying package.json
    Modifying package.json  for test coverage and reporting

---
+++ src/_tests_/App.spec.js
@@ -0,0 +1,3 @@
+import react from "react";
+import { render,cleanup, fireEvent } from '@testing-library/react';
+import { Checkbox } from "../components/Checkbox";
--- src/_tests_/Checkbox.spec.js
+++ src/_tests_/Checkbox.spec.js
@@ -9,7 +9,7 @@ jest.mock('../firebase',() => ({
         firestore: jest.fn(() => ({
             collection: jest.fn(() => ({
                 doc: jest.fn(() => ({
-                    update: ject.fn(),
+                    update: jest.fn(),
                 })),
             })),
         })),
@@ -18,12 +18,30 @@ jest.mock('../firebase',() => ({
 
 describe('<Checkbox />',() => {
     describe('Success',() => {
-        if('renders the task checkbox' , () => {
+        it('renders the task checkbox' , () => {
             const {queryByTestId} = render(
                 <Checkbox id="1" taskDesc="Finish this tutorial series" />
             );
             expect(queryByTestId('checkbox-action')).toBeTruthy(); 
         });
+
+        it('renders the task checkbox and accepts a onClick' , () => {
+            const {queryByTestId} = render(
+                <Checkbox id="1" taskDesc="Finish this tutorial series" />
+            );
+            expect(queryByTestId('checkbox-action')).toBeTruthy(); 
+            fireEvent.click(queryByTestId('checkbox-action'));
+        });
+        it('renders the task checkbox and accepts a onKeyDown' , () => {
+            const {queryByTestId} = render(
+                <Checkbox id="1" taskDesc="Finish this tutorial series" />
+            );
+            expect(queryByTestId('checkbox-action')).toBeTruthy(); 
+            fireEvent.keyDown(queryByTestId('checkbox-action'));
+            
+        });
+
+
 
     });
 });
