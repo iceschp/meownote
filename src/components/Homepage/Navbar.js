b27e272339fca388492f3f5227a8d86209957454
iceschp <iceschp>
2021-12-07 21:58:35 +0700

    add Hompage folder

---
+++ src/components/Homepage/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const Navbar = () => {
+    const test = 1;
+
+    return (
+        <div className="Navbar">
+        <h1>Navbar</h1>
+        </div>
+    )
+};
