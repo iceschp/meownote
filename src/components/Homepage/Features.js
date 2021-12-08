af78920821a07ce029939e1e1ba0a19a9dedcb05
iceschp <iceschp>
2021-12-08 16:40:22 +0700

    add content of feature page

---
+++ src/components/Homepage/Features.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class Features extends Component {
+    render() {
+        return (
+            <div className="feature__box">
+                <h1>Features</h1>
+                <div className="row">
+                    <div className="column">
+                        <h2>Organizaed</h2>
+                        <p>A lot of folders as much as you want</p>
+                    </div>
+                    <div className="column">
+                        <h2>Productivity</h2>
+                        <p>Work smart and Fast</p>
+                    </div>
+                    <div className="column">
+                        <h2>Share</h2>
+                        <p>Sharing to another app</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
